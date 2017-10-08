1043995de6576ae8a828c0adc3578f14c68ca245
culiuy <culiuy@163.com>
2017-10-08 11:54:40 +0800

    zuul增加过滤器

--- src/main/java/com/cu/tvapi/TvapiApplication.java
+++ src/main/java/com/cu/tvapi/TvapiApplication.java
@@ -3,6 +3,9 @@ package com.cu.tvapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.cu.tvapi.filter.AccessFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +13,13 @@ public class TvapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TvapiApplication.class, args);
+	}
+	
+	/**
+	 * 增加过滤器
+	 * */
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
